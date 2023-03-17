7af144be35b08bee9a8da2f0f393fc6925f5bd18
Josh Boustead <josh.boustead@pemdastech.com>
2023-03-17 10:22:01 -0500

    Updated for connecting to Render.

--- config/Database.php
+++ config/Database.php
@@ -1,33 +1,36 @@
 <?php
 
 class Database {
-    private $host = 'localhost';
-    private $port = '5432';
-    private $db_name = 'quotesdb';
-    private $username = 'boustead';
-    private $password = 'none';
+    private $host;
+    private $port;
+    private $dbname;
+    private $username;
+    private $password;
     private $conn;
 
-//    public function __construct() {
-//        $this->username = getenv('USERNAME');
-//        $this->password = getenv('PASSWORD');
-//        $this->db_name = getenv('DBNAME');
-//        $this->host= getenv('HOST');
-//        $this->port = getenv('PORT');
-//    }
-
-    public function connect() {
-        $this->conn = null;
-        $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
-
-        try {
-            $this->conn = new PDO ($dsn, $this->username, $this->password);
-
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            echo 'Connection Error: ' . $e->getMessage();
-        }
-        return $this->conn;
+    public function __construct() {
+        $this->username = getenv('USERNAME');
+        $this->password = getenv('PASSWORD');
+        $this->dbname = getenv('DBNAME');
+        $this->host= getenv('HOST');
+        $this->port = getenv('PORT');
     }
 
+    public function connect()
+    {
+        //$this->conn = null;
+        if ($this->conn) {
+            //connection already exists, return it
+            return $this->conn;
+        } else {
+            $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname};";
+            try {
+                $this->conn = new PDO ($dsn, $this->username, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                return $this->conn;
+            } catch (PDOException $e) {
+                echo 'Connection Error: ' . $e->getMessage();
+            }
+        }
+    }
 }
---
+++ index.php
@@ -0,0 +1,6 @@
+<html>
+<h1>Joshua Boustead</h1>
+<h2>Mid-term Project Spring 2023</h2>
+<h2>Fort Hays State University</h2>
+</html>
+<?php
