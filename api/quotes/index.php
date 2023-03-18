be321021ca88c6befde5c1a97cf2860e3aeaf86a
Josh Boustead <josh.boustead@pemdastech.com>
2023-03-18 01:47:38 -0500

    Fixed an issue with quotes index.php file.

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -5,6 +5,8 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
+$method = $_SERVER['REQUEST_METHOD'];
+
 if ($method === 'OPTIONS') {
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
     header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
@@ -12,12 +14,6 @@ if ($method === 'OPTIONS') {
 }
 
 // Get the method request and the id if provided for all the tables
-
-if (isset($_SERVER['REQUEST_METHOD'])) {
-    $method = $_SERVER['REQUEST_METHOD'];
-} else {
-    $method = null;
-}
 
 if (isset($_GET['id'])) {
     $quoteId = ($_GET['id']);
